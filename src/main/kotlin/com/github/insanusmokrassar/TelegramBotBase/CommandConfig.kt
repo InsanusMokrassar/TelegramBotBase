50f2c97ab678ce49d8ceb52c34b1afcb3bde3399
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-04-29 20:25:42 +0800

    added onBotInit callback and baseHandleIObject

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotBase/CommandConfig.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotBase/CommandConfig.kt
@@ -1,5 +1,3 @@
 package com.github.insanusmokrassar.TelegramBotBase
 
-import com.github.insanusmokrassar.ConfigsRemapper.ConfigWrapper
-
-open class CommandConfig(val command: String) : ConfigWrapper()
+open class CommandConfig(val command: String)
